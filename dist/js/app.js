92376515d4b7489dcd7467749d870b181417660d
Erick Castilho <ecastilho@marinformatica.com.br>
2023-01-24 17:57:41 -0300

    abc

---
+++ dist/js/app.js
@@ -0,0 +1,10 @@
+import Negociacao from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+
+
+console.log(negociacao.data);
+
+//negociacao.quantidade = 1000;
+
+console.log(negociacao.volume);
